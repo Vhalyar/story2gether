28cf2900c26acfbcfcd15b7f353bf8f3823ee236
Vhalyar <165473972+Vhalyar@users.noreply.github.com>
2025-06-26 18:09:58 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -8,82 +8,61 @@ app.use(express.static("public"));
 let players = [];
 let story = [];
 let turnIndex = 0;
-let currentBoxIndex = 0;
 let voteInProgress = false;
 let votes = {};
 
 function resetGame() {
-  players.forEach(p => {
-    p.ready = false;
-  });
+  players.forEach(p => p.ready = false);
   story = [];
   turnIndex = 0;
-  currentBoxIndex = 0;
   voteInProgress = false;
   votes = {};
-  io.emit("playersUpdate", players);
 }
 
 io.on("connection", (socket) => {
   socket.on("setName", (name) => {
     if (!name || players.find(p => p.name === name)) return;
-    const color = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    const color = "#" + Math.floor(Math.random()*16777215).toString(16);
     const player = { id: socket.id, name, color, ready: false };
     players.push(player);
-    socket.emit("playerData", { ...player });
+    socket.emit("playerData", player);
     io.emit("playersUpdate", players);
   });
 
-  socket.on("readyStatus", (status) => {
+  socket.on("toggleReady", () => {
     const player = players.find(p => p.id === socket.id);
-    if (player) {
-      player.ready = status;
-      io.emit("playersUpdate", players);
-      if (players.length > 1 && players.every(p => p.ready)) {
-        players = players.sort(() => 0.5 - Math.random());
-        players.forEach((p, i) => p.turnIndex = i);
-        turnIndex = 0;
-        currentBoxIndex = 0;
-        story = [];
-        players.forEach(p => {
-          io.to(p.id).emit("gameStart", {
-            players,
-            myTurnIndex: p.turnIndex,
-            turnIndex,
-            currentBoxIndex,
-            story
-          });
-        });
-      }
+    if (!player) return;
+    player.ready = !player.ready;
+    io.emit("playersUpdate", players);
+    if (players.length > 1 && players.every(p => p.ready)) {
+      const shuffled = [...players].sort(() => 0.5 - Math.random());
+      shuffled.forEach((p, i) => { p.turnOrder = i; });
+      turnIndex = 0;
+      io.emit("gameStart", { players: shuffled, turnIndex, story });
     }
   });
 
   socket.on("submitText", (text) => {
-    const player = players[turnIndex];
-    if (!player || socket.id !== player.id || voteInProgress) return;
-
+    const player = players.find(p => p.id === socket.id);
+    if (!player || player.turnOrder !== turnIndex || voteInProgress) return;
     story.push({ text, playerName: player.name, color: player.color });
     turnIndex = (turnIndex + 1) % players.length;
-    currentBoxIndex++;
-    io.emit("storyUpdate", { story, turnIndex, currentBoxIndex });
+    io.emit("storyUpdate", { story, turnIndex });
   });
 
   socket.on("voteToEnd", () => {
-    if (!voteInProgress) {
-      voteInProgress = true;
-      votes = {};
-      io.emit("voteInitiated");
-    }
+    if (voteInProgress) return;
+    voteInProgress = true;
+    votes = {};
+    io.emit("voteStarted");
   });
 
   socket.on("castVote", (choice) => {
     const player = players.find(p => p.id === socket.id);
     if (!player || votes[player.id]) return;
     votes[player.id] = choice;
-    const total = Object.keys(votes).length;
-    if (total === players.length) {
-      const allAgree = Object.values(votes).every(v => v === "agree");
-      if (allAgree) {
+    if (Object.keys(votes).length === players.length) {
+      if (Object.values(votes).every(v => v === "agree")) {
         io.emit("gameEnded", story);
       } else {
         voteInProgress = false;
@@ -96,6 +75,7 @@ io.on("connection", (socket) => {
   socket.on("restartGame", () => {
     resetGame();
     io.emit("gameRestarted");
+    io.emit("playersUpdate", players);
   });
 
   socket.on("disconnect", () => {
